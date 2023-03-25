6c0931a6934231f5496d0eb52ac8a08694322c76
HeetShah <heetshah7642@gmail.com>
2023-03-24 19:15:46 -0600

    login func

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -13,20 +13,44 @@
   ```
 */
 import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const login = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        <Navigate to="/dashboard" />;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
-    <button
-      type="submit"
-      className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-    >
-      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-        <LockClosedIcon
-          className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-          aria-hidden="true"
-        />
-      </span>
-      Sign in
-    </button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <button onClick={login}>Sign in with Google</button>
+        </div>
+      </div>
+    </div>
   );
 }
