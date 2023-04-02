4eedc25bdffee0a6e4aa4eeea6bc0be4f28b32c4
Anmol Tyagi <anmolty2@gmail.com>
2023-04-01 22:32:51 -0400

    fix index.tsx imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+
 import Landing from "./components/Landing";
 import reportWebVitals from "./reportWebVitals";
 
