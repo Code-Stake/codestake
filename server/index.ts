b5afcc6effec7a3a2a3dfeacf83867954c9deb62
HeetShah <heetshah7642@gmail.com>
2023-04-11 20:28:38 -0600

    fetch question from firebase db

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,14 @@ const fs = require("fs");
 app.use(cors());
 const server = http.createServer(app);
 
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'codestake-3a83c'
+});
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -68,67 +76,78 @@ app.get("/", (req, res) => {
 
 app.get("/api/questionContents", (req, res) => {
   try {
-    const fileContents = fs.readFileSync("questionContent.txt", "utf-8");
-    const sectionPlaceholders = [
-      {
-        start: "# <section1>",
-        end: "# </section1>",
-        name: "preProcessingCode",
-      },
-      {
-        start: "# <section2>",
-        end: "# </section2>",
-        name: "starterCode",
-      },
-      {
-        start: "# <section3>",
-        end: "# </section3>",
-        name: "executionCode",
-      },
-      {
-        start: "# <section4>",
-        end: "# </section4>",
-        name: "testCaseInput1",
-      },
-      {
-        start: "# <section5>",
-        end: "# </section5>",
-        name: "testCaseOutput1",
-      },
-      {
-        start: "# <section6>",
-        end: "# </section6>",
-        name: "testCaseInput2",
-      },
-      {
-        start: "# <section7>",
-        end: "# </section7>",
-        name: "testCaseOutput2",
-      },
-      {
-        start: "# <section8>",
-        end: "# </section8>",
-        name: "testCaseInput3",
-      },
-      {
-        start: "# <section9>",
-        end: "# </section9>",
-        name: "testCaseOutput3",
-      },
-    ];
-    const result = {};
-    for (const placeholder of sectionPlaceholders) {
-      const regex = new RegExp(
-        `${placeholder.start}([\\s\\S]*?)${placeholder.end}`,
-        "s"
-      );
-      const match = fileContents.match(regex);
-      const contents = match ? match[1] : "";
-      result[placeholder.name] = contents;
-    }
-    return res.send(result);
+    
+    const bucket = admin.storage().bucket();
+
+    const file = bucket.file('questions/reverseLinkedList.txt');
+
+    file.download().then((contents) => {
+
+      const fileContents = contents.toString();
+      const sectionPlaceholders = [
+        {
+          start: "# <section1>",
+          end: "# </section1>",
+          name: "preProcessingCode",
+        },
+        {
+          start: "# <section2>",
+          end: "# </section2>",
+          name: "starterCode",
+        },
+        {
+          start: "# <section3>",
+          end: "# </section3>",
+          name: "executionCode",
+        },
+        {
+          start: "# <section4>",
+          end: "# </section4>",
+          name: "testCaseInput1",
+        },
+        {
+          start: "# <section5>",
+          end: "# </section5>",
+          name: "testCaseOutput1",
+        },
+        {
+          start: "# <section6>",
+          end: "# </section6>",
+          name: "testCaseInput2",
+        },
+        {
+          start: "# <section7>",
+          end: "# </section7>",
+          name: "testCaseOutput2",
+        },
+        {
+          start: "# <section8>",
+          end: "# </section8>",
+          name: "testCaseInput3",
+        },
+        {
+          start: "# <section9>",
+          end: "# </section9>",
+          name: "testCaseOutput3",
+        },
+      ];
+      const result = {};
+      for (const placeholder of sectionPlaceholders) {
+        const regex = new RegExp(
+          `${placeholder.start}([\\s\\S]*?)${placeholder.end}`,
+          "s"
+        );
+        const match = fileContents.match(regex);
+        const contents = match ? match[1] : "";
+        result[placeholder.name] = contents;
+      }
+      return res.send(result);
+    }).catch((error) => {
+      return res.send(error);
+    });
+    
   } catch (error) {
-    res.send(error);
+    return res.send(error);
   }
 });
 
