52d2b9274a902a6ccd23211b723cf83a82605fd3
Khush Patel <k425pate@uwaterloo.ca>
2023-04-11 20:31:51 -0400

    Server-side + read from external file + 3 test cases + organized code

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,158 +1,99 @@
 // Landing.js
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CodeEditorWindow from "./CodeEditorWindow";
 import axios from "axios";
 import { classnames } from "../utils/general";
 import { languageOptions } from "../constants/languageOptions";
 import "../index.css";
-
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { defineTheme } from "../lib/defineThemes";
-import useKeyPress from "../hooks/useKeyPress";
-// import Footer from "./Footer";
 import OutputWindow from "./OutputWindow";
 import CustomInput from "./CustomInput";
 import OutputDetails from "./OutputDetails";
 import ThemeDropdown from "./ThemeDropdown";
 import LanguagesDropdown from "./LanguagesDropdown";
-import { app } from "../firebase";
-import {
-  getFirestore,
-  doc,
-  collection,
-  getDocs,
-  query,
-  getDoc,
-} from "firebase/firestore";
-
-// const javascriptDefault = `// some comment`;
 
 type themeType = { value: string; label: string } | string;
 
-type languages = {
-  Python3: string;
-  JS?: string;
-  Java?: string;
-};
-
-type Questions = {
-  prompt: string;
-  starterCode: languages;
-  config: string;
-};
-
-const QUESTIONS: { [key: string]: Questions } = {
-  // LL: {
-  //   prompt: "Reverse this Linked List",
-  //   starterCode: {
-  //     Python3:
-  //       "def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:",
-  //   },
-  //   config: "
-  //   ",
-  // },
-  // TREE: {},
-  ARR: {
-    prompt: "Reverse this Array",
-    starterCode: {
-      Python3: "def reverseArray(nums):",
-    },
-    config: "\nimport json\ns = input()\nprint(validParentheses(s))\n",
-  },
-  ARRwithINT: {
-    prompt: "Two sum ",
-    starterCode: {
-      Python3: "def twoSum(nums: List[int], target: int) -> List[int]:",
-    },
-    config: `
-  import json
-
-  s = input()
-  nums = json.loads(s.split()[0])
-  target = int(s.split()[1])
-`,
-  },
-};
-
 const Landing = () => {
-  const [code, setCode] = useState("");
-
+  // UI related states
   const [customInput, setCustomInput] = useState("");
   const [outputDetails, setOutputDetails] = useState(null);
   const [processing, setProcessing] = useState<boolean | null>(null);
   const [theme, setTheme] = useState<themeType>("cobalt");
   const [language, setLanguage] = useState(languageOptions[37]);
-  const [input, setInput] = useState("");
-  const db = getFirestore(app);
 
-  // firebase consts
-  const [questionData, setQuestionData] = useState({});
-  const [testCaseInput, setTestCaseInput] = useState("");
-  const [testCaseOuput, setTestCaseOutput] = useState("");
-  const [prompt, setPrompt] = useState("");
-  const [executionCode, setExecutionCode] = useState("");
-  const [classCode, setClassCode] = useState("");
+  //  Code related states
+  const [questionContents, setQuestionContents] = useState<any>({});
+  const [preProcessingCode, setPreProcessingCode] = useState<any>("");
+  const [code, setCode] = useState("");
+  const [testCaseInput, setTestCaseInput] = useState("[1,2,3,4]");
+  const [testCaseOuput, setTestCaseOutput] = useState("[4, 3, 2, 1]");
+  const [testCaseInputs, setTestCaseInputs] = useState<any>({});
+  const [testCaseOutputs, setTestCaseOutputs] = useState<any>({});
 
-  const enterPress = useKeyPress("Enter");
-  const ctrlPress = useKeyPress("Control");
+  const [exeCode, setExeCode] = useState<any>("");
 
+  // Initial GET request to get the question content
   useEffect(() => {
-    getQuestionData();
+    getQuestionContent();
   }, []);
 
+  // UI related functions
   const onSelectChange = (sl: any) => {
     console.log("selected Option...", sl);
     setLanguage(sl);
   };
-
-  const getQuestionData = async () => {
-    try {
-      const docRef = doc(collection(db, "solutions"), "reverse-linked-list");
-      const docSnap = await getDoc(docRef);
-      console.log("docSnap", docSnap.data());
-      if (!docSnap.exists()) throw new Error("No such document!");
-      setQuestionData(docSnap.data());
-      setCode(
-        docSnap
-          .data()
-          .starterCode.python3.replace(/\\n/g, "\n")
-          .replace(/\\t/g, "\t")
-      );
-      console.log(docSnap.data().starterCode.python3);
-      console.log(code);
-      if (docSnap.data().testCases[1]?.arrayRepInput)
-        setTestCaseInput(docSnap.data().testCases[1].arrayRepInput);
-      else setTestCaseInput(docSnap.data().testCases[1].input);
-      setTestCaseOutput(docSnap.data().testCases[1].output);
-      setExecutionCode(
-        docSnap
-          .data()
-          .executionCode.python3.replace(/\\n/g, "\n")
-          .replace(/\\t/g, "\t")
-      );
-      setPrompt(docSnap.data().prompt);
-      if (docSnap.data().classCode)
-        setClassCode(
-          docSnap
-            .data()
-            .classCode.python3.replace(/\\n/g, "\n")
-            .replace(/\\t/g, "\t")
-        );
-    } catch (error) {
-      console.log("error", error);
-    }
+  useEffect(() => {
+    defineTheme("oceanic-next").then((_) =>
+      setTheme({ value: "oceanic-next", label: "Oceanic Next" })
+    );
+  }, []);
+  function handleThemeChange(th: any) {}
+  const showSuccessToast = (msg: any) => {
+    toast.success(msg || `Compiled Successfully!`, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const showErrorToast = (msg?: any) => {
+    toast.error(msg || `Something went wrong! Please try again.`, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
 
-  useEffect(() => {
-    if (enterPress && ctrlPress) {
-      console.log("enterPress", enterPress);
-      console.log("ctrlPress", ctrlPress);
-      // handleCompile();
-    }
-  }, [ctrlPress, enterPress]);
+  // Code related functions
+  const getQuestionContent = () => {
+    axios.get("http://localhost:3001/api/questionContents").then((response) => {
+      setQuestionContents(response.data);
+      setPreProcessingCode(response.data["preProcessingCode"]);
+      setCode(response.data["starterCode"].trim());
+      setExeCode(response.data["executionCode"]);
+      setTestCaseInputs({
+        1: response.data["testCaseInput1"].trim(),
+        2: response.data["testCaseInput2"].trim(),
+        3: response.data["testCaseInput3"].trim(),
+      });
+      setTestCaseOutputs({
+        1: response.data["testCaseOutput1"].trim(),
+        2: response.data["testCaseOutput2"].trim(),
+        3: response.data["testCaseOutput3"].trim(),
+      });
+    });
+  };
   const onChange = (action: any, data: any) => {
     switch (action) {
       case "code": {
@@ -164,19 +105,14 @@ const Landing = () => {
       }
     }
   };
-
-  console.log(code);
-  let configCode = classCode + code + executionCode;
-  console.log(configCode);
-
-  const handleCompile = () => {
+  const handleCompile = (input: string, output: string) => {
     setProcessing(true);
+    setTestCaseInput(input);
     const formData = {
       language_id: language.id,
-      // encode source code in base64
-      expected_output: btoa(testCaseOuput),
-      source_code: btoa(configCode),
-      stdin: btoa(testCaseInput),
+      expected_output: btoa(output),
+      source_code: btoa(preProcessingCode + code + exeCode),
+      stdin: btoa(input),
     };
     const options = {
       method: "POST",
@@ -204,7 +140,6 @@ const Landing = () => {
         console.log(error);
       });
   };
-
   const checkStatus = async (token: any) => {
     const options = {
       method: "GET",
@@ -239,40 +174,6 @@ const Landing = () => {
       showErrorToast();
     }
   };
-
-  function handleThemeChange(th: any) {
-    // We will come to the implementation later in the code
-  }
-  useEffect(() => {
-    defineTheme("oceanic-next").then((_) =>
-      setTheme({ value: "oceanic-next", label: "Oceanic Next" })
-    );
-  }, []);
-
-  const showSuccessToast = (msg: any) => {
-    toast.success(msg || `Compiled Successfully!`, {
-      position: "top-right",
-      autoClose: 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
-  const showErrorToast = (msg?: any) => {
-    toast.error(msg || `Something went wrong! Please try again.`, {
-      position: "top-right",
-      autoClose: 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
-
-  console.log(code);
 
   if (code === "") {
     return <div>Loading...</div>;
@@ -320,14 +221,40 @@ const Landing = () => {
                 setCustomInput={setCustomInput}
               />
               <button
-                onClick={handleCompile}
+                onClick={() =>
+                  handleCompile(testCaseInputs[1], testCaseOutputs[1])
+                }
                 disabled={!code}
                 className={classnames(
                   "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
                   !code ? "opacity-50" : ""
                 )}
               >
-                {processing ? "Processing..." : "Compile and Execute"}
+                {processing ? "Processing..." : "Test Case 1"}
+              </button>
+              <button
+                onClick={() =>
+                  handleCompile(testCaseInputs[2], testCaseOutputs[2])
+                }
+                disabled={!code}
+                className={classnames(
+                  "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                  !code ? "opacity-50" : ""
+                )}
+              >
+                {processing ? "Processing..." : "Test Case 2"}
+              </button>
+              <button
+                onClick={() =>
+                  handleCompile(testCaseInputs[3], testCaseOutputs[3])
+                }
+                disabled={!code}
+                className={classnames(
+                  "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                  !code ? "opacity-50" : ""
+                )}
+              >
+                {processing ? "Processing..." : "Test Case 3"}
               </button>
             </div>
             {outputDetails && <OutputDetails outputDetails={outputDetails} />}
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ const app = express();
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+const fs = require("fs");
 
 app.use(cors());
 const server = http.createServer(app);
@@ -14,64 +15,121 @@ const io = new Server(server, {
   },
 });
 
-
-
 io.on("connection", (socket) => {
   console.log("A user connected: " + socket.id);
 
-  const roomCapacity = {}
-  
+  const roomCapacity = {};
+
   socket.on("join_room", (data) => {
     const room = data;
     let numUsers = io.sockets.adapter.rooms.get(room)?.size || 0;
-    
-    if (numUsers < 2) {
-      
-      socket.join(room)
-      
-      numUsers = io.sockets.adapter.rooms.get(room)?.size
 
+    if (numUsers < 2) {
+      socket.join(room);
+
+      numUsers = io.sockets.adapter.rooms.get(room)?.size;
 
       //emit to all users in the room
-      io.to(room).emit("room_joined", { message: `User joined room ${room}` ,room: room, capacity: `${numUsers}` });
-        roomCapacity[room] = numUsers      
-
-
+      io.to(room).emit("room_joined", {
+        message: `User joined room ${room}`,
+        room: room,
+        capacity: `${numUsers}`,
+      });
+      roomCapacity[room] = numUsers;
     } else {
       socket.emit("room_full", { message: "Room is already full!" });
     }
-
-  } );
-
+  });
 
   socket.on("send_message", (data) => {
     socket.to(data.roomEntered).emit("receive_message", data);
   });
 
-
-    // Listen to the 'disconnect' event on the socket
-    socket.on("disconnect", () => {
-      console.log(`Socket disconnected: ${socket.id}`);
-      // You can remove the socket from any rooms it's joined
-      const rooms = Object.keys(socket.rooms);
-      rooms.forEach((room) => {
-        if (room !== socket.id) {
-          socket.leave(room);
-          // Decrement the room capacity when the socket leaves the room
-          roomCapacity[room] -= 1;
-          // Emit the room capacity to all sockets in the room
-          // io.to(room).emit("room_capacity", { capacity: `${roomCapacity[room]}/2` })
-
-        }
-      });
+  // Listen to the 'disconnect' event on the socket
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected: ${socket.id}`);
+    // You can remove the socket from any rooms it's joined
+    const rooms = Object.keys(socket.rooms);
+    rooms.forEach((room) => {
+      if (room !== socket.id) {
+        socket.leave(room);
+        // Decrement the room capacity when the socket leaves the room
+        roomCapacity[room] -= 1;
+        // Emit the room capacity to all sockets in the room
+        // io.to(room).emit("room_capacity", { capacity: `${roomCapacity[room]}/2` })
+      }
     });
-
+  });
 });
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/questionContents", (req, res) => {
+  try {
+    const fileContents = fs.readFileSync("questionContent.txt", "utf-8");
+    const sectionPlaceholders = [
+      {
+        start: "# <section1>",
+        end: "# </section1>",
+        name: "preProcessingCode",
+      },
+      {
+        start: "# <section2>",
+        end: "# </section2>",
+        name: "starterCode",
+      },
+      {
+        start: "# <section3>",
+        end: "# </section3>",
+        name: "executionCode",
+      },
+      {
+        start: "# <section4>",
+        end: "# </section4>",
+        name: "testCaseInput1",
+      },
+      {
+        start: "# <section5>",
+        end: "# </section5>",
+        name: "testCaseOutput1",
+      },
+      {
+        start: "# <section6>",
+        end: "# </section6>",
+        name: "testCaseInput2",
+      },
+      {
+        start: "# <section7>",
+        end: "# </section7>",
+        name: "testCaseOutput2",
+      },
+      {
+        start: "# <section8>",
+        end: "# </section8>",
+        name: "testCaseInput3",
+      },
+      {
+        start: "# <section9>",
+        end: "# </section9>",
+        name: "testCaseOutput3",
+      },
+    ];
+    const result = {};
+    for (const placeholder of sectionPlaceholders) {
+      const regex = new RegExp(
+        `${placeholder.start}([\\s\\S]*?)${placeholder.end}`,
+        "s"
+      );
+      const match = fileContents.match(regex);
+      const contents = match ? match[1] : "";
+      result[placeholder.name] = contents;
+    }
+    return res.send(result);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 server.listen(3001, () => {
